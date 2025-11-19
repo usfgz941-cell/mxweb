6ad268bd036f34a26c55c608ef28e3658e20f9e0
usfgz941-cell <usfgz941@gmail.com>
2025-11-19 16:17:43 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mxweb/',  // <--- 新增了这一行，用于指定 GitHub Pages 的路径
+  base: '/',  // <--- 修改这里：改成 '/' 适配 Vercel
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
